38c523ff634b9aef7c4258c32886c3a1d0f5aa61
Anurag Anand <anuraganand7618@gmail.com>
2022-05-14 18:45:27 +0530

    initial commit

---
+++ Module1/errorstream.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cerr << "An Un-buffered standard error stream (cerr) occured"<<endl;
+	clog << "An buffered standard error stream (clog) occured";
+	
+}

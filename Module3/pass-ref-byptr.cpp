ca6e72fdc79548226dab5478d2f53031aefcd8fd
Anurag Anand <anuraganand7618@gmail.com>
2022-05-18 18:14:26 +0530

    Create pass-ref-byptr.cpp

---
+++ Module3/pass-ref-byptr.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void swapbyptr(int *x, int *y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+void swapbyref(int &x, int &y)
+{
+	int temp = x;
+	x = y;
+	y = temp;
+	
+}
+
+int main()
+{
+	int a = 45, b = 35;
+	
+	swapbyptr(&a, &b);
+	cout<< a <<" " << b <<endl;
+	
+	swapbyref(a,b);
+	cout<<a <<" "<< b<<endl;
+	
+	swapbyref(a,b);
+	cout<<a <<" "<< b<<endl;
+}
